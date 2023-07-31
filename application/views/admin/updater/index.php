c470ff2b37fa7f695bcdd6ec9fb4860fb6d94660
Pitti Sunil Kumar <88083221+pittisunilkumar@users.noreply.github.com>
2023-07-31 21:33:29 +0530

    Update index.php

--- application/views/admin/updater/index.php
+++ application/views/admin/updater/index.php
@@ -100,7 +100,7 @@
                             }
                             ?>
 <div class="font15">
-<?php echo $this->lang->line('please_check') ?> <a href="https://smart-school.in/category/changelog/smart-school" target="_blank" class="displayinline align-text-top font-weight-bold"><?php echo $this->lang->line('changelog') ?></a> <?php echo $this->lang->line('for_latest_version_update') ?> 
+<?php echo $this->lang->line('please_check') ?> <a href="https://www.crm.cyberdetox.in/authentication/login" target="_blank" class="displayinline align-text-top font-weight-bold"><?php echo $this->lang->line('changelog') ?></a> <?php echo $this->lang->line('for_latest_version_update') ?> 
     
 </div>
                         </div><!--./row-->
